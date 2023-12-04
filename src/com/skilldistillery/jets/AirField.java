4b8c510517f82c8dc7060e4f81841ece61278f4b
Samantha <someartbysam@gmail.com>
2023-12-04 06:31:21 -0700

    Jets project

--- src/com/skilldistillery/jets/AirField.java
+++ src/com/skilldistillery/jets/AirField.java
@@ -3,16 +3,41 @@ package com.skilldistillery.jets;
 import java.util.ArrayList;
 
 public class AirField {
-	private ArrayList<Jet> jets = new ArrayList<>();
+	
+	public ArrayList<Jet> jets = new ArrayList<>();
+	
+//	public AirField(ArrayList<Jet> jets) {
+//		super();
+//		this.jets = jets;
+//	}
 	
 	public void addJetList() {
 	Jet voyager = new passengerJet("OM95", 90000.5, 15000, 95867345);
-	Jet enterprise = new explorerJet("OG50", 90001.8, 15500, 123445693);
-	Jet klingon = new fighterJet("K19c4", 80002.3, 14003, 12000034); //(model, speed, price, range)
+	Jet enterprise1 = new explorerJet("OG50", 90001.8, 15500, 123445693);
+	Jet enterprise2 = new explorerJet("OG50", 90001.8, 15500, 123445693);
+	Jet klingon1 = new fighterJet("K19c4", 80002.3, 14003, 12000034); //(model, speed, price, range)
+	Jet klingon2 = new fighterJet("K19c4", 80002.3, 14003, 12000034); //(model, speed, price, range)
 
 	jets.add(voyager); //passenger
-	jets.add(enterprise); //explorer
-	jets.add(klingon); //fighter
+	jets.add(enterprise1); //explorer
+	jets.add(enterprise2); 
+	jets.add(klingon1); //fighter
+	jets.add(klingon2);
+	
+	for(Jet jetName : jets) {
+		System.out.println(jetName);
+	}
+	
+	
 	
 	}
+
+//	public ArrayList<Jet> getJets() {
+//		return jets;
+//	}
+//
+//	public void setJets(ArrayList<Jet> jets) {
+//		this.jets = jets;
+//	}
+	
 }
--- src/com/skilldistillery/jets/JetsApp.java
+++ src/com/skilldistillery/jets/JetsApp.java
@@ -1,36 +1,94 @@
 package com.skilldistillery.jets;
 
+import java.util.ArrayList;
 import java.util.Scanner;
-
+//Story1
 public class JetsApp {
 	
-	private AirField airField;
-	 // call other methods here
+	public AirField airField;
+		
+		
 	
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
 		JetsApp launch = new JetsApp();
+		AirField airField1 = new AirField();
+		airField1.addJetList();
 		
 		// Greeting
 		System.out.println("Hello! Welcome to the Jet App");
-		launch.displayMenu();
+		//Story3
+		launch.displayMenu(userInput);
 		
 	}
 	
 	//Story 4
-	public void displayMenu() {
+	public void displayMenu(Scanner userInput) {
 		
-		System.out.println("Please enter the number corresponding with your selection: ");
-		System.out.println("1. List fleet");
-		System.out.println("2. Fly all jets");
-		System.out.println("3. View the fastest jet");
-		System.out.println("4. View Jet with the longest range");
-		System.out.println("5. Prepare the BEAMS");
-		System.out.println("6. Load Plasma Cannons");
-		System.out.println("7. Add a Jet to Fleet");
-		System.out.println("8. Remove a Jet from Fleet");
-		System.out.println("9. Quit");
+		int userChoice = 0;
+		
+		while(userChoice != 9) {
+			System.out.println("Please enter the number corresponding with your selection: ");
+			System.out.println("1. List fleet");
+			System.out.println("2. Fly all jets");
+			System.out.println("3. View the fastest jet");
+			System.out.println("4. View Jet with the longest range");
+			System.out.println("5. Prepare the BEAMS");
+			System.out.println("6. Load Plasma Cannons");
+			System.out.println("7. Add a Jet to Fleet");
+			System.out.println("8. Remove a Jet from Fleet");
+			System.out.println("9. Quit");
+			userChoice = userInput.nextInt();
+			userInteraction(userChoice);
+		}
 		
 	}
+	
+	public void userInteraction(int userinput) {
+		
+		switch (userinput) {
+		case 1:
+			displayJets(airField.jets);
+			break;
+		case 2:
+			System.out.println("Flying all Jets!");
+			break;
+		case 3: 
+			System.out.println("The fastest Jet on record is: ");
+			break;
+		case 4:
+			System.out.println("The jet with the longest range is:");
+			break;
+		case 5:
+			System.out.println("");
+			break;
+		case 6:
+			System.out.println("Plasma Cannons loaded!");
+			break;
+		case 7:
+			System.out.println("Which jet would you like to remove?");
+			break;
+		case 8:
+			System.out.println("Enter the details for the jet you would like to add: ");
+			break;
+		case 9:
+			System.out.println("Have a wonderful day!");
+			break;
+		default: 
+			System.out.println("Please enter a valid number.");
+			break;
+		}
+		
+	}
+	
+	public void displayJets(ArrayList<Jet> jets) {
+		for(Jet jet : jets ) {
+			if(jet != null) {
+				System.out.println(jet);
+			}
+		}	
+	}
+	
+	
 
 }
